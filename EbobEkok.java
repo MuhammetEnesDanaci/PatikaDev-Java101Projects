de37aaaf0e21bf5229bd27f0b2fd9d8e536b13fa
MuhammetEnesDanaci <39264603+MuhammetEnesDanaci@users.noreply.github.com>
2022-12-28 14:06:26 +0300

    Add files via upload

---
+++ EbobEkok.java
@@ -0,0 +1,40 @@
+package patikaUygulamalar;
+
+import java.util.Scanner;
+
+public class EbobEkok {
+
+	public static void main(String[] args) {
+		
+		int sayi,sayi2,EBOB = 1,EKOK = 1, i = 2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hoşgeldiniz...");
+        System.out.print("Lütfen 1. sayiyi giriniz :"); sayi = sc.nextInt();
+        System.out.print("Lütfen 2. sayiyi giriniz :"); sayi2 = sc.nextInt();
+
+        if (sayi < 1 || sayi2 < 1){
+            System.out.println("Girdiğiniz sayılardan en az bir tanesi 1'den küçük.");
+            System.out.println("Çıkış yapılıyor...");
+        }else {
+            while (sayi > 1 || sayi2 > 1){
+                if(sayi%i == 0 && sayi2%i == 0){
+                    EBOB *=i;
+                    EKOK *=i;
+                    sayi /=i;
+                    sayi2 /=i;
+                } else if (sayi%i == 0 && sayi2%i != 0 ) {
+                    EKOK *=i;
+                    sayi /= i;
+                } else if (sayi%i != 0 && sayi2%i == 0) {
+                    EKOK *=i;
+                    sayi2 /= i;
+                } else {
+                    i++;
+                }
+            }
+            System.out.println("EBOB :" + EBOB + " EKOK :" + EKOK);
+        }
+
+	}
+
+}
